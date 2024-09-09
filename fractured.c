dc164202ddb71488253704a600ac7233718f191c
nathanguevara11 <nathangeagle@hotmail.com>
2024-09-08 23:26:44 -0400

    Add files via upload

--- fractured.c
+++ fractured.c
@@ -11,31 +11,12 @@ double askinput()
     scanf("%lf", &input);
     return input; 
 }
-// double scan1()
-// {
-//     printf("Give me two numbers to use as coordinates\n"); 
-//     double x1, x2;
-//     scanf("%lf %lf", &x1, &x2);
-//     return (x2-x1);
-// }
 
-
-// double scan2()
- 
-// {
-//     printf("give me two numbers to use as another set of coordinates\n"); 
-//     double y3, y2;
-//     scanf("%lf %lf", &y3, &y2);
-//}
 
 
 
 double distance_equation()
 { 
-    // printf("Give me two numbers.\n"); 
-    // double call = scan1(); 
-    // printf("give me two more numbers.\n"); 
-    // double back = scan2(); 
     printf("Give me two numbers to use as coordinates\n"); 
     static double x1, y1;
     x1 = askinput();
@@ -48,32 +29,27 @@ double distance_equation()
     printf("Your coordinates are (%.2f, %.2f) and (%.2f, %.2f)\n", x1, y1, x2, y2); 
 
     double distance = sqrt(pow((x2-x1), 2)) + sqrt(pow((y2-y1), 2)); 
-    printf("the distance betweem the two points is %.2f\n", distance); 
+    printf("the distance between the two points is %.2f\n", distance); 
     return distance;
 }
 
-// double print_distance()
-// {
-//     double dise = distance_equation();
-//     printf("Your distance is %.2f\n", dise);
-//     return 3; 
-// } 
 double perimeter_equation()
 {
-
-    //double distan = distance_equation(); 
     double distan = distance_equation(); 
     double perimeter = (2 * PI * (distan/ 2));
     printf("The perimeter of this circle is %.2f\n", perimeter);
 
     return perimeter; 
 }
-double calc_width(){
+
+double calc_width()
+{
     double dist = distance_equation();  
-    double width = (dist / 2);
+    double width = dist;
     printf("Width of this circle is %.2f\n", width); 
     return width; 
 }
+
 double area()
 {
     double wid = distance_equation();
@@ -82,15 +58,17 @@ double area()
     printf("The area of this circle is %.2f\n", area);
     return 3; 
 }
+
 double height() 
 {
     double hig = distance_equation(); 
     printf("height of this circle is %.2f\n", hig);
     return 3;
 }
+
 int main(){
 
-// askinput(); 
+ 
 distance_equation();
 perimeter_equation(); 
 area(); 
